f087d411f3053498230049772b5b68abd336c417
Hamza Zarfaoui <83501010+HamzaZF@users.noreply.github.com>
2025-06-02 11:56:31 +0800

    current_user

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_key_vault" "adb_kv" {
   access_policy {
     # tenant_id = data.azurerm_client_config.current.tenant_id
     # object_id = data.azurerm_client_config.current.object_id
-    tenant_id = data.azuread_user.current_user.tenant_id
+    tenant_id = data.azuread_client_config.current.tenant_id
     object_id = data.azuread_user.current_user.object_id
 
     secret_permissions = [
